345f771878e9c1209f36f6f566d269a21ee4c90e
David Araya <david.araya@alumnos.ucn.cl>
2022-12-10 23:21:55 -0300

    feat: DataFrame class updated to support multi csv dataframes in class

--- DataFrame.py
+++ DataFrame.py
@@ -1,8 +1,9 @@
 import configparser
 import pandas
 
+
 class DataFrame:
-    dict = {
+    default_values = {
         'fips': False,
         'date': False,
         'state': 'CONSTRUCTOR',
@@ -18,52 +19,66 @@ class DataFrame:
         'cases_f': False,
     }
 
-    def __init__(self, path_file):
-        try:
-            if path_file == "resources/counties_us.csv":
-                self.dataframe = pandas.read_csv(path_file, delimiter='/')
-            else:
-                self.dataframe = pandas.read_csv(path_file)
-            self.file_exists = True
+    def __init__(self, **kwargs: str):
+        # First create the minimum class attributes to work
+        self.file_exists = False
+        self.dataframes = dict()
+        # **kwargs replace key-value array with csv path and their names,
+        # thus we don't need a paired key-value structure to assign in the class dict
+        for key, value in kwargs.items():
+            self.init_dataframe(key, value)
 
-        except FileNotFoundError:
-            self.file_exists = False
+    def init_dataframe(self, file_name, path_file):
+        if path_file == 'resources/states_us.csv':
+            self.dataframes[file_name] = pandas.read_csv(path_file, delimiter=',')
+        elif path_file == 'resources/daily_cases_us.csv':
+            self.dataframes[file_name] = pandas.read_csv(path_file, delimiter=',')
+        elif path_file == 'resources/counties_us.csv':
+            self.dataframes[file_name] = pandas.read_csv(path_file, delimiter='/')
+        else:
+            raise ValueError("The path_file: '%s' is not in the list" % path_file)
+        self.file_exists = True
 
-    def normalize(self):
-        if not self.file_exists:
-            raise FileNotFoundError
+    def to_string(self):
+        for dataframe in self.dataframes.values():
+            print(dataframe)
 
-        self.dataframe.columns = self.dataframe.columns.str.lower()
 
-        if 'state' in self.dataframe.columns:
-            self.dataframe['state'] = self.dataframe['state'].str.upper()
-
-        if 'county' in self.dataframe.columns:
-            self.dataframe['county'] = self.dataframe['county'].str.upper()
-
-        if 'date' in self.dataframe.columns:
-            self.dataframe['date'] = self.dataframe['date'].str.replace('!%', '/')
-            self.dataframe = self.dataframe[self.dataframe.county != 'UNKNOWN']
-            self.dataframe['deaths'] = self.dataframe['deaths'].fillna('NULL')
-
-        if 'fips' in self.dataframe.columns:
-            self.dataframe['fips'] = self.dataframe['fips'].astype(str)
-
-        if self.dataframe.iloc[319]['county'] == "DISTRICT OF COLUMBIA":
-            self.dataframe.loc[319, 'state_code'] = "DC"
-            self.dataframe[['state_code']] = self.dataframe[['state_code']].fillna('PR')
-
-        self.dataframe.rename(
-            columns={'state_code': 'code', 'county': 'name', 'male': 'male_population', 'female': 'female_population',
-                     'median_age': 'average_age', 'lat': 'latitude', 'long': 'longitude', 'cases_m': 'male_cases',
-                     'cases_f': 'female_cases'}, inplace=True)
-
-    def verify_population(self):
-        if not self.file_exists:
-            raise FileNotFoundError
-        self.dataframe['suma población'] = self.dataframe.iloc[:, [4, 5]].sum(axis=1)
-        #print(self.dataframe)
-        for i in self.dataframe.index:
-            if self.dataframe['population'][i] != self.dataframe['suma población'][i]:
-                print("The county with different population is : ",self.dataframe['name'][i])
-                return False
+    # def normalize(self):
+    #     if not self.file_exists:
+    #         raise FileNotFoundError
+    #
+    #     self.dataframe.columns = self.dataframe.columns.str.lower()
+    #
+    #     if 'state' in self.dataframe.columns:
+    #         self.dataframe['state'] = self.dataframe['state'].str.upper()
+    #
+    #     if 'county' in self.dataframe.columns:
+    #         self.dataframe['county'] = self.dataframe['county'].str.upper()
+    #
+    #     if 'date' in self.dataframe.columns:
+    #         self.dataframe['date'] = self.dataframe['date'].str.replace('!%', '/')
+    #         self.dataframe = self.dataframe[self.dataframe.county != 'UNKNOWN']
+    #         self.dataframe['deaths'] = self.dataframe['deaths'].fillna('NULL')
+    #
+    #     if 'fips' in self.dataframe.columns:
+    #         self.dataframe['fips'] = self.dataframe['fips'].astype(str)
+    #
+    #     if self.dataframe.iloc[319]['county'] == "DISTRICT OF COLUMBIA":
+    #         self.dataframe.loc[319, 'state_code'] = "DC"
+    #         self.dataframe[['state_code']] = self.dataframe[['state_code']].fillna('PR')
+    #
+    #     self.dataframe.rename(
+    #         columns={'state_code': 'code', 'county': 'name', 'male': 'male_population', 'female': 'female_population',
+    #                  'median_age': 'average_age', 'lat': 'latitude', 'long': 'longitude', 'cases_m': 'male_cases',
+    #                  'cases_f': 'female_cases'}, inplace=True)
+    #
+    # def verify_population(self):
+    #     if not self.file_exists:
+    #         raise FileNotFoundError
+    #     self.dataframe['suma población'] = self.dataframe.iloc[:, [4, 5]].sum(axis=1)
+    #     # print(self.dataframe)
+    #     for i in self.dataframe.index:
+    #         if self.dataframe['population'][i] != self.dataframe['suma población'][i]:
+    #             print("The county with different population is : ", self.dataframe['name'][i])
+    #             return False
--- main.py
+++ main.py
@@ -2,30 +2,37 @@ import configparser
 from DataFrame import DataFrame
 import pandas as pd
 
+
 # from Connect import Connect
 # from RDB import RDB
 # from MDB import MDB
 # from Cube import Cube
 
 
+def allan_method():
+    # I've working on DataFrame class, so, temporally this block of code will not run
+    pass
+    # config = configparser.ConfigParser()
+    # config.read('resources/config.ini')
+    # paths = config['paths']
+    # if config['database'].getboolean('normalize'):
+    #     dataframe_1 = DataFrame(paths['path_file_counties_us'])
+    #     dataframe_1.normalize()
+    #     dataframe_2 = DataFrame(paths['path_file_states_us'])
+    #     dataframe_2.normalize()
+    #     dataframe_3 = DataFrame(paths['path_file_daily_cases_us'])
+    #     dataframe_3.normalize()
+    #
+    #     merge_dataframe = pd.merge(dataframe_1.dataframe, dataframe_2.dataframe, how="outer")
+    #     merge_dataframe.to_csv("resources/merge.csv", index=False, encoding='utf-8-sig')
+    #     verify_Dataframe = DataFrame(paths['path_file_merge'])
+    #     verify_Dataframe.verify_population()
+    #     print(dataframe_1.dataframe)
+    #     print(dataframe_2.dataframe)
+    #     print(dataframe_3.dataframe)
+
+
 if __name__ == '__main__':
-    config = configparser.ConfigParser()
-    config.read('resources/config.ini')
-    paths = config['paths']
-    if config['database'].getboolean('normalize'):
-
-        dataframe_1 = DataFrame(paths['path_file_counties_us'])
-        dataframe_1.normalize()
-        dataframe_2 = DataFrame(paths['path_file_states_us'])
-        dataframe_2.normalize()
-        dataframe_3 = DataFrame(paths['path_file_daily_cases_us'])
-        dataframe_3.normalize()
-
-        merge_dataframe = pd.merge(dataframe_1.dataframe, dataframe_2.dataframe, how="outer")
-        merge_dataframe.to_csv("resources/merge.csv", index=False, encoding='utf-8-sig')
-        verify_Dataframe = DataFrame(paths['path_file_merge'])
-        verify_Dataframe.verify_population()
-
-        print(dataframe_1.dataframe)
-        print(dataframe_2.dataframe)
-        print(dataframe_3.dataframe)
+    dataframe = DataFrame(states_us='resources/states_us.csv', daily_cases_us='resources/daily_cases_us.csv',
+                          counties_us='resources/counties_us.csv')
+    print(dataframe.to_string())
