e6a9bbf17d2f629733d16dc88b33334420a839d9
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2022-12-13 22:56:39 -0300

    caso4

--- CaseFour.py
+++ CaseFour.py
@@ -11,15 +11,29 @@ class CaseFour:
         self.connect = Connect('mdb')
 
     def view(self):
-        statement = 'SELECT c.name, c.fips FROM county_dimension c, state_dimension s WHERE c.state_code = code AND ' \
-                    's.name = \'' + self.state + '\''
+        statement = 'SELECT code, c.fips FROM county_dimension c, state_dimension s WHERE c.state_code = code AND ' \
+                    's.name = \'' + self.state + '\' ORDER BY c.fips ASC'
         table = self.connect.select(statement)
         if not table:
             return
         for row in table:
             self.counties.append(row[0])
             self.fips.append(row[1])
+        cases_states = []
+        for fips in self.fips:
+            statement = 'SELECT female_cases, male_cases FROM facts WHERE ' \
+                        'county_fips = ' + str(fips)
+            table = self.connect.select(statement)
+            cases_counties = []
+            for row in table:
+                cases_counties.append(row[0] + row[1])
+            cases_states.append(cases_counties)
 
-
+        plt.rcParams['figure.autolayout'] = True
+        fig, ax = plt.subplots()
+        ax.boxplot(cases_states)
+        ax.set_xticks([])
+        ax.set_title('Gráfico de cajas y bigotes de cada estado')
+        plt.show()
 
 
